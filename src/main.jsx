f0be3de467d52890274ba8b4180c4fd3637ac28d
reza hagju <ansherlireza2020@gmail.com>
2024-10-02 18:50:04 +0330

    changes the uri of client variable for error

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  uri: import.meta.VITE_GRAPHCMS_URL,
+  uri: import.meta.env.VITE_GRAPHCMS_URL,
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    {/* <StrictMode> */}
+    <App />
+    {/* </StrictMode> */}
   </ApolloProvider>
 );
