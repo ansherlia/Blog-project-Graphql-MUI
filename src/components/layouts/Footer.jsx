968274bd7b796c597a72e3a5614a3d5dd1aa0af8
reza hagju <ansherlireza2020@gmail.com>
2024-10-02 14:30:01 +0330

    developed navbar website

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,18 @@
-
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import BookIcon from "@mui/icons-material/Book";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography component="h5" variant="h4" fontWeight={700} flex={1}>
+            Blog Project
+          </Typography>
+          <BookIcon />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Footer;
