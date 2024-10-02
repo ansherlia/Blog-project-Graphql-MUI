7fa5d63ebc77ff20ee1e2663badd91cb71927688
reza hagju <ansherlireza2020@gmail.com>
2024-10-02 14:41:32 +0330

    developed header project

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,6 +1,21 @@
+import { AppBar, Typography } from "@mui/material";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Typography
+      sx={{
+        backgroundColor: "#1976d3",
+        textAlign: "center",
+        color: "#fff",
+        marginTop: "100px",
+      }}
+      fontSize={25}
+      p={1.7}
+      fontWeight={600}
+    >
+      This is a Blog project with graphql & graphCMS & MUI
+    </Typography>
+  );
 }
 
 export default Header;
