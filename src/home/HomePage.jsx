33ab4b047dbfff1585893aa11d95ee3022ad542b
reza hagju <ansherlireza2020@gmail.com>
2024-10-02 17:46:16 +0330

    developed homepage.jsx

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,5 +1,36 @@
+import { Container, Grid, Typography } from "@mui/material";
+import Author from "../components/authors/Author";
+import Blogs from "../components/blogs/Blogs";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Container maxWidth="lg">
+      <Grid container padding={3} mt={2} spacing={5}>
+        <Grid item>
+          <Typography
+            fontSize="2.5rem"
+            fontWeight={700}
+            variant="h4"
+            component="h5"
+          >
+            Authors
+          </Typography>
+          <Author />
+        </Grid>
+        <Grid item ml="120px">
+          <Typography
+            fontSize="2.5rem"
+            fontWeight={700}
+            variant="h4"
+            component="h5"
+          >
+            Blogs
+          </Typography>
+          <Blogs />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default HomePage;
