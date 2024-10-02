e6d59f2834336779f74b1c4bda9cba1b43e991f5
reza hagju <ansherlireza2020@gmail.com>
2024-10-02 18:02:57 +0330

    created client variable in main.jsx for connect react project to graphql

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+const client = new ApolloClient({
+  uri: import.meta.VITE_GRAPHCMS_URL,
+  cache: new InMemoryCache(),
+});
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>
+);
