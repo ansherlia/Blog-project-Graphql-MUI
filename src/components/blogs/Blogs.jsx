f9005f1bf28b4f4e699c1ed2a847514677788715
reza hagju <ansherlireza2020@gmail.com>
2024-10-03 12:09:28 +0330

    the data was taken from hygraph and looped over it

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,5 +1,23 @@
+import { useQuery } from "@apollo/client";
+import { GET_BLOGS_INFO } from "../../graphql/queries";
+import { Grid } from "@mui/material";
+import CardEle from "../../shared/CardEle";
+
 function Blogs() {
-  return <div>Blogs</div>;
+  const { loading, data, error } = useQuery(GET_BLOGS_INFO);
+  console.log({ loading, data });
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>{error.message}</h2>;
+  const { posts } = data;
+  return (
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item key={post.id} maxWidth="lg" xs={12} sm={6} md={4}>
+          <CardEle {...post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default Blogs;
