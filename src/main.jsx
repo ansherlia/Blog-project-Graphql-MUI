53ecec3d289e88e4fd0ad05109fafe2e06ffad24
reza hagju <ansherlireza2020@gmail.com>
2024-10-03 12:53:16 +0330

    wraped broweserRouter and deleted react.strictMOde

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHCMS_URL,
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")).render(
-  <ApolloProvider client={client}>
-    {/* <StrictMode> */}
-    <App />
-    {/* </StrictMode> */}
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
